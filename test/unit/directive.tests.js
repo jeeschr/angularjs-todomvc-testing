de0fee41237a478a83b81996604943e2eeda61ba
jeeschr <jeschror@gmail.com>
2016-02-11 16:22:50 -0500

    add directive test

--- test/unit/directive.tests.js
+++ test/unit/directive.tests.js
@@ -3,12 +3,29 @@
  */
 
 
-describe('todomvc todoFocus', function() {
+describe('todomvc - todoFocus ->', function() {
 
 	beforeEach(module('todoFocus'));
 
-	var scope;
+	var $rootScope, $compile, $scope, elem, $timeout;
 
+	beforeEach(inject(function(_$compile_, _$rootScope_, _$timeout_){
+		$rootScope = _$rootScope_;
+		$timeout = _$timeout_;
+		$scope = $rootScope.$new();
+		$compile = _$compile_;
+		elem = angular.element('<input todo-focus="focus">');
+		$compile(elem)($scope);
+		$scope.focus=true;
+		$scope.$digest();
+	}));
+
+	it('should focus on true expression evaluation', function(){
+		spyOn(elem[0], 'focus');
+		$timeout.flush();
+		// console.log('!elem: ', elem);
+		expect(elem[0].focus).toHaveBeenCalled();
+	});
 
 });
 
